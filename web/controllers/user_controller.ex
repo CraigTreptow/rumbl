d4c55a2691de83fc686ca8509321e7061bfb65ec
Craig Treptow <craig.treptow@gmail.com>
2018-10-01 19:34:17 -0500

    users can register, login, and logout

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,4 +1,7 @@
-defmodule Rumbl.UserController do use Rumbl.Web, :controller
+defmodule Rumbl.UserController do
+  use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
+
   alias Rumbl.User
 
   def index(conn, _params) do
@@ -20,11 +23,24 @@ defmodule Rumbl.UserController do use Rumbl.Web, :controller
     changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
-      {:ok, user} -> conn
+      {:ok, user} ->
+        conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+        |> put_flash(:error, "You must be logged in to access that page")
+        |> redirect(to: page_path(conn, :index))
+        |> halt()
     end
   end
 end
