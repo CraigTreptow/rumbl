8e67049442650b6d4510a2fc7d336a734971ecd5
Craig Treptow <craig.treptow@gmail.com>
2018-10-09 18:13:23 -0500

    Context integration tests

--- test/rumbl/accounts/accounts_test.exs
+++ test/rumbl/accounts/accounts_test.exs
@@ -1,5 +1,6 @@
 defmodule Rumbl.AccountsTest do
   use Rumbl.DataCase
+
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
@@ -9,7 +10,6 @@ defmodule Rumbl.AccountsTest do
       username: "eva",
       credential: %{email: "eva@test", password: "secret"}
     }
-
     @invalid_attrs %{}
 
     test "with valid data inserts user" do
@@ -28,44 +28,48 @@ defmodule Rumbl.AccountsTest do
     test "enforces unique usernames" do
       assert {:ok, %User{id: id}} = Accounts.register_user(@valid_attrs)
       assert {:error, changeset} = Accounts.register_user(@valid_attrs)
+
       assert %{username: ["has already been taken"]} = errors_on(changeset)
       assert [%User{id: ^id}] = Accounts.list_users()
     end
 
     test "does not accept long usernames" do
       attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
-      {:error, changeset} = Accounts.register_user(attrs)
-  
+      assert  {:error, changeset} = Accounts.register_user(attrs)
+
       assert %{username: ["should be at most 20 character(s)"]} = errors_on(changeset)
       assert Accounts.list_users() == []
     end
 
     test "requires password to be at least 6 chars long" do
       attrs = put_in(@valid_attrs, [:credential, :password], "12345")
-      {:error, changeset} = Accounts.register_user(attrs)
+      assert  {:error, changeset} = Accounts.register_user(attrs)
 
       assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)[:credential]
-      assert Accounts.list_users() == []
+      assert  Accounts.list_users() == []
     end
   end
 
   describe "authenticate_by_email_and_pass/2" do
     @email "user@localhost"
     @pass "123456"
+
     setup do
       {:ok, user: user_fixture(email: @email, password: @pass)}
     end
 
     test "returns user with correct password", %{user: %User{id: id}} do
-      assert {:ok, %User{id: ^id}} = Accounts.authenticate_by_email_and_pass(@email, @pass)
+      assert {:ok, %User{id: ^id}} =
+             Accounts.authenticate_by_email_and_pass(@email, @pass)
     end
 
     test "returns unauthorized error with invalid password" do
-      assert {:error, :unauthorized} = Accounts.authenticate_by_email_and_pass(@email, "badpass")
+      assert {:error, :unauthorized} =
+             Accounts.authenticate_by_email_and_pass(@email, "badpass")
     end
 
     test "returns not found error with no matching user for email" do
       assert {:error, :not_found} = Accounts.authenticate_by_email_and_pass("bademail@localhost", @pass)
     end
-  end
+   end
 end
--- test/rumbl/multimedia/multimedia_test.exs
+++ test/rumbl/multimedia/multimedia_test.exs
@@ -5,26 +5,28 @@ defmodule Rumbl.MultimediaTest do
 
   describe "categories" do
     test "list_alphabetical_categories/0" do
-      for name <- ~w(Drama Action Comedy), do: Multimedia.create_category(name)
+      for name <- ~w(Sci-fi Romance Drama Action Comedy), do: Multimedia.create_category(name)
 
       alpha_names =
         for %Category{name: name} <- Multimedia.list_alphabetical_categories() do
           name
         end
 
-      assert alpha_names == ~w(Action Comedy Drama)
+      assert alpha_names == ~w(Action Comedy Drama Romance Sci-fi)
     end
   end
 
   describe "videos" do
     alias Rumbl.Multimedia.Video
+
     @valid_attrs %{description: "desc", title: "title", url: "http://local"}
     @invalid_attrs %{description: nil, title: nil, url: nil}
 
     test "list_videos/0 returns all videos" do
       owner = user_fixture()
       %Video{id: id1} = video_fixture(owner)
-      assert [%Video{id: ^id1}] = Multimedia.list_videos() %Video{id: id2} = video_fixture(owner)
+      assert [%Video{id: ^id1}] = Multimedia.list_videos()
+      %Video{id: id2} = video_fixture(owner)
       assert [%Video{id: ^id1}, %Video{id: ^id2}] = Multimedia.list_videos() end
 
 
@@ -36,7 +38,8 @@ defmodule Rumbl.MultimediaTest do
 
     test "create_video/2 with valid data creates a video" do
       owner = user_fixture()
-      assert {:ok, %Video{} = video} = Multimedia.create_video(owner, @valid_attrs) assert video.description == "desc"
+      assert {:ok, %Video{} = video} = Multimedia.create_video(owner, @valid_attrs)
+       assert video.description == "desc"
       assert video.title == "title"
       assert video.url == "http://local"
     end
@@ -49,20 +52,23 @@ defmodule Rumbl.MultimediaTest do
     test "update_video/2 with valid data updates the video" do
       owner = user_fixture()
       video = video_fixture(owner)
-      assert {:ok, video} = Multimedia.update_video(video, %{title: "updated title"}) assert %Video{} = video
+      assert {:ok, video} = Multimedia.update_video(video, %{title: "updated title"})
+      assert %Video{} = video
       assert video.title == "updated title"
     end
 
     test "update_video/2 with invalid data returns error changeset" do
       owner = user_fixture()
       %Video{id: id} = video = video_fixture(owner)
-      assert {:error, %Ecto.Changeset{}} = Multimedia.update_video(video, @invalid_attrs) assert %Video{id: ^id} = Multimedia.get_video!(id)
+      assert {:error, %Ecto.Changeset{}} = Multimedia.update_video(video, @invalid_attrs)
+      assert %Video{id: ^id} = Multimedia.get_video!(id)
     end
 
     test "delete_video/1 deletes the video" do
       owner = user_fixture()
       video = video_fixture(owner)
-      assert {:ok, %Video{}} = Multimedia.delete_video(video) assert Multimedia.list_videos() == []
+      assert {:ok, %Video{}} = Multimedia.delete_video(video)
+      assert Multimedia.list_videos() == []
     end
 
     test "change_video/2 returns a video changeset" do
